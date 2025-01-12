33e3ef02e6a78e54223224076da2443b449a794e
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-01-12 23:46:20 +0100

    [feat] Add iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -3,11 +3,40 @@
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
-// Dichiara la funzione qui.
+// Dichiara la funzione qui
+
+function firstLetter(namesInArray) {
+    
+
+// Crea nuovo array
+
+const newNames = [];
+
+
+// Crea ciclo sull'array originario
+
+for (let i = 0; i < namesInArray.length; i++) {
+
+    let element = namesInArray[i];
+
+    let elementTwo = element.charAt(0);
+
+    newNames.push(elementTwo);
+    
+}
+
+return newNames;
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+firstLetter(names);
+
+console.log(firstLetter(names));
 
 
-//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+
+// Risultato atteso: ["A", "L", "M", "A", "G", "A"]
