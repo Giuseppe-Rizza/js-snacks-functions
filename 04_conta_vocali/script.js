ec66ff0bfa13b276dd7018819238989f1535bda1
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-01-13 00:24:08 +0100

    [feat] Add conta_vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -3,11 +3,38 @@
 const word = 'javascript';
 
 
-// Dichiara la funzione qui.
+// Dichiara la funzione qui
+
+function countVowels(string) {
+
+    let vowels = 0;
+
+
+    // Inserisci ciclo
+
+    for (let i = 0; i < string.length; i++) {
+
+        let type = string.charAt(i);
+
+        if (["a", "e", "i", "o", "u"].includes(type)) {
+
+            vowels += 1;
+            
+        }
+
+      
+    }
+
+    return vowels;
+  }  
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+countVowels(word);
+
+console.log(countVowels(word));
 
 
-//Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+// Risultato atteso se si passa 'javascript': 3 (a, a, i)
